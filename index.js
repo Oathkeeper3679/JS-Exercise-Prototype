6bfa3fc85808127b8bcaae36f8cd93e625d86039
Oathkeeper3679 <jonanthorose2@yahoo.com>
2019-12-11 20:32:04 -0600

    Shaun-Stevens

--- index.js
+++ index.js
@@ -39,9 +39,26 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+Person.prototype.eat = function (someFood) {
+  if (this.stomach.length < 10){
+    this.stomach.push(someFood);
+  }
+};
+Person.prototype.poop = function () {
+  return '${this.stomach[]}';
+};
+Person.prototype.toString = function () {
+  return '${this.name}, ${this.age}';
+};
+
+
+
+
 
 /*
   TASK 2
@@ -57,8 +74,15 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
 
+Car.prototype.fill = function(gallons){
+  this.tank += gallons;
 }
 
 /*
@@ -68,18 +92,34 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function(){
+  return 'Playing with ${this.favoriteToy}';
+};
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window/Global Object Binding
+      -The value of 'this' will be the window or console
+        Object.
+
+  2. Implicit Binding
+      -Preceding dot(.) calls a function, the object before the dot(.) is 'this'.
+
+  3. New Binding
+      -Constructor function is used, 'this' refers to a SPECIFIC instance of the object that is created and returned.
+
+  4. Explicit Binding
+      -Call and Apply method is used in new functions for 'this' to be EXPLICITLY defined.
 */
 
 
